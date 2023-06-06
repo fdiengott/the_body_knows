834d84ca89b99489939d0e9d6f0616c2697b5a8d
fdiengott <freddy.diengott@proton.me>
2023-06-05 23:59:20 -0400

    add intersection observer above the navbar

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,52 @@
-<script setup>
-import { RouterView } from 'vue-router';
-import NavBar from './components/NavBar.vue';
-</script>
-
 <template>
   <div class="app-wrapper">
-    <NavBar />
+    <div id="header-scroll-listener"></div>
+    <NavBar :sticking="sticking" />
     <RouterView />
   </div>
 </template>
 
+<script>
+import { RouterView } from 'vue-router';
+import NavBar from './components/NavBar.vue';
+
+window.addEventListener('load', () => {
+  const handleIntersect = () => {
+    // console.log('intersecting');
+
+  }
+  const options = {
+    // root: null
+  };
+
+  const observer = new IntersectionObserver(handleIntersect, options);
+
+  observer.observe(document.querySelector('#header-scroll-listener'));
+})
+
+export default {
+
+  setup() {
+    return {
+      sticking: false
+    }
+  },
+  components: { RouterView, NavBar }
+}
+</script>
+
 <style scoped>
-/* nav {
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-} */
+.app-wrapper {
+  position: relative;
+  height: 100vh;
+  overflow-y: auto;
+  overflow-x: hidden;
+  perspective: 10px;
+
+  display: grid;
+  grid-template-rows: 0 80px 1fr;
+
+  scroll-behavior: smooth;
+  scroll-padding-top: 8%;
+}
 </style>
