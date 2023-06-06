b13af7d53b13ffc883d4e60db3297b7d0a8e660a
fdiengott <freddy.diengott@proton.me>
2023-06-05 23:55:31 -0400

    add eslint packages

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,10 @@
 /* eslint-env node */
+// require('@babel/eslint-parser')
 require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-prettier/skip-formatting'
