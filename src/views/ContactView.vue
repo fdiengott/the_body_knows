4eb3f11f94c290482bf822274778ec1efb6b8f97
fdiengott <freddy.diengott@proton.me>
2023-06-22 23:17:10 -0400

    add netlify attribute to contact form

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -12,7 +12,7 @@ export default {
         <div class="section-wrapper">
             <h1>Contact</h1>
             <p>I would love to connect with you for a free 30 minute intro call.</p>
-            <form action="">
+            <form data-netlify="true" method="POST" name="contact">
                 <fieldset class="form-group">
                     <input type="text" name="name" id="name-input" required placeholder=" ">
                     <label for="name-input">Name</label>
