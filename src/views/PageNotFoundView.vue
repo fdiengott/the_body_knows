560d89cbabdbaa9a69cc98c91e285b76be6f44f2
fdiengott <freddy.diengott@proton.me>
2023-06-28 09:27:39 -0400

    add a 404 page

---
+++ src/views/PageNotFoundView.vue
@@ -0,0 +1,10 @@
+<template lang="">
+    <div class="form-submit-card-wrapper">
+        <div class="form-submit-card">
+            <h1>Oops</h1>
+            <p>It the looks like the page you have requested doesn't exist.</p>
+            <div class="vertical-offset"></div>
+            <RouterLink class="return-home" to="/">Home</RouterLink>
+        </div>
+    </div>
+</template>
